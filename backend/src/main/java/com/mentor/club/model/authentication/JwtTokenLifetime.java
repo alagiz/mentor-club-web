545cce4e6e0dbb13820b78f251c281115a1cf579
ArtemAlagizov <alagizov@gmail.com>
2020-12-06 11:35:28 +0100

    changed lifetime of access token to 10 minutes

--- backend/src/main/java/com/mentor/club/model/authentication/JwtTokenLifetime.java
+++ backend/src/main/java/com/mentor/club/model/authentication/JwtTokenLifetime.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 public enum JwtTokenLifetime {
     PASSWORD_RESET_TOKEN_LIFESPAN_IN_SECONDS(24L * 3600L),    // 24 hours
     REFRESH_TOKEN_LIFESPAN_IN_SECONDS(30L * 24L * 3600L),     // 30 days
-    ACCESS_TOKEN_LIFESPAN_IN_SECONDS(1L * 3600L);             // 1 hour
+    ACCESS_TOKEN_LIFESPAN_IN_SECONDS(10L * 60L);              // 10 minutes
 
     @Getter
     private final Long lifetime;
