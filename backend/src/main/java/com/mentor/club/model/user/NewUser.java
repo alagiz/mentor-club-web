862214cdf3a83f772fd5da6c4a5163b2ddb8ba78
ArtemAlagizov <alagizov@gmail.com>
2020-12-14 12:02:07 +0100

    minor change

--- backend/src/main/java/com/mentor/club/model/user/NewUser.java
+++ backend/src/main/java/com/mentor/club/model/user/NewUser.java
@@ -4,11 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 public class NewUser {
     @Getter
     @Setter
